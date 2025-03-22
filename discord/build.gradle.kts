c15f91de364c54fd2ad4946137b09c36c9d3aef1
Ryder Belserion <no-reply@ryderbelserion.com>
2025-03-22 12:55:43 -0400

    add discord module

--- discord/build.gradle.kts
+++ discord/build.gradle.kts
@@ -1,11 +1,18 @@
 plugins {
     id("root-plugin")
+
+    alias(libs.plugins.shadow)
 }
+
+project.group = "${rootProject.group}.discord"
+project.version = rootProject.version
+project.description = "A version of Fusion for Discord bots!"
 
 repositories {
 
 }
 
 dependencies {
-
+    implementation(libs.logback)
+    implementation(libs.jda)
 }
