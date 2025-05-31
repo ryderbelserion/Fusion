804aaa7af27d388007b772dc4062a5646e171f8e
Ryder Belserion <contact@ryderbelserion.com>
2025-05-30 23:32:16 -0400

    Add javadocs to component builder

--- core/src/main/java/com/ryderbelserion/fusion/kyori/components/builders/ComponentBuilder.java
+++ core/src/main/java/com/ryderbelserion/fusion/kyori/components/builders/ComponentBuilder.java
@@ -20,6 +20,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Builds a component to send to a player.
+ *
+ * @author ryderbelserion
+ */
 public class ComponentBuilder {
 
     private final FusionKyori kyori = (FusionKyori) FusionCore.Provider.get();
@@ -29,26 +34,54 @@ public class ComponentBuilder {
 
     private final Audience target;
 
+    /**
+     * Creates the component builder an audience provided.
+     *
+     * @param target the audience target
+     * @param line   the lines of text for the component
+     */
     public ComponentBuilder(@NotNull final Audience target, @NotNull final String line) {
         this(target, new ArrayList<>() {{
             add(line);
         }});
     }
 
+    /**
+     * Creates the component builder an audience provided.
+     *
+     * @param target the audience target
+     * @param lines  the text for the component
+     */
     public ComponentBuilder(@NotNull final Audience target, @NotNull final List<String> lines) {
         this.lines.addAll(lines);
         this.target = target;
     }
 
+    /**
+     * Creates the component builder with no audience provided.
+     *
+     * @param lines the lines of text for the component
+     */
     public ComponentBuilder(@NotNull final List<String> lines) {
         this(Audience.empty(), lines);
     }
 
+    /**
+     * Creates the component builder with no audience provided.
+     *
+     * @param line the text for the component
+     */
     public ComponentBuilder(@NotNull final String line) {
         this(Audience.empty(), line);
     }
 
-    public List<Component> asComponents(@NotNull final Map<String, String> placeholders) {
+    /**
+     * Returns a list of components with configurable placeholders.
+     *
+     * @param placeholders a map of placeholders
+     * @return {@link Component}
+     */
+    public @NotNull final List<Component> asComponents(@NotNull final Map<String, String> placeholders) {
         final List<Component> components = new ArrayList<>(this.lines.size());
 
         final List<TagResolver> resolvers = this.resolvers.values().stream().flatMap(List::stream).toList();
@@ -58,35 +91,90 @@ public class ComponentBuilder {
         return components;
     }
 
-    public Component asComponent(@NotNull final Map<String, String> placeholders) {
+    /**
+     * Returns the first component with configurable placeholders.
+     *
+     * @param placeholders a map of placeholders
+     * @return {@link Component}
+     */
+    public @NotNull final Component asComponent(@NotNull final Map<String, String> placeholders) {
         return asComponents(placeholders).getFirst();
     }
 
-    public List<Component> asComponents() {
+    /**
+     * Returns a list of components with no placeholders.
+     *
+     * @return {@link Component}
+     */
+    public @NotNull final List<Component> asComponents() {
         return asComponents(new HashMap<>());
     }
 
-    public Component asComponent() {
+    /**
+     * Returns the first component with no placeholders
+     *
+     * @return {@link Component}
+     */
+    public @NotNull final Component asComponent() {
         return asComponents(new HashMap<>()).getFirst();
     }
 
-    public ComponentBuilder addClickResolver(@NotNull final String key, @NotNull final String value, @NotNull final ClickEvent.Action action) {
+    /**
+     * Adds a click resolver to the list.
+     *
+     * @param key    the key
+     * @param value  the action on click
+     * @param action the click action type
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder addClickResolver(@NotNull final String key, @NotNull final String value, @NotNull final ClickEvent.Action action) {
         return addResolver(ResolverType.CLICK_EVENT, key, value, action);
     }
 
-    public ComponentBuilder addPlaceholderResolver(@NotNull final String key, @NotNull final String value) {
+    /**
+     * Adds a placeholder resolver to the list.
+     *
+     * @param key   the key
+     * @param value the value
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder addPlaceholderResolver(@NotNull final String key, @NotNull final String value) {
         return addResolver(ResolverType.PLACEHOLDER, key, value, null);
     }
 
-    public ComponentBuilder addHoverResolver(@NotNull final String key, @NotNull final String value) {
+    /**
+     * Adds a hover resolver to the list.
+     *
+     * @param key   the key
+     * @param value the hover text
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder addHoverResolver(@NotNull final String key, @NotNull final String value) {
         return addResolver(ResolverType.HOVER_EVENT, key, value, null);
     }
 
-    public ComponentBuilder addColorResolver(@NotNull final String key, @NotNull final String value) {
+    /**
+     * Adds a text color resolver to the list.
+     *
+     * @param key the key
+     * @param value the color
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder addColorResolver(@NotNull final String key, @NotNull final String value) {
         return addResolver(ResolverType.TEXT_COLOR, key, value, null);
     }
 
-    public ComponentBuilder addResolver(@NotNull final ResolverType type, @NotNull final String key, @NotNull final String value, @Nullable final ClickEvent.Action action) {
+    /**
+     * Adds a resolver to the list with a configurable key/value and optional click event action
+     * This method will switch between placeholders, text colors, click events, and hover events.
+     *
+     * @param type the type of resolver
+     * @param key the placeholder or id
+     * @param value the value
+     * @param action the click action
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder addResolver(@NotNull final ResolverType type, @NotNull final String key, @NotNull final String value, @Nullable final ClickEvent.Action action) {
         final List<TagResolver> resolvers = this.resolvers.getOrDefault(type, new ArrayList<>());
 
         switch (type) {
@@ -111,7 +199,7 @@ public class ComponentBuilder {
             case HOVER_EVENT -> {
                 @NotNull final HoverEvent<Component> event = HoverEvent.showText(AdvUtils.parse(value));
 
-                final TagResolver.@NotNull Single placeholder = Placeholder.styling(key, event);
+                @NotNull final TagResolver.@NotNull Single placeholder = Placeholder.styling(key, event);
 
                 resolvers.add(placeholder);
             }
@@ -122,7 +210,13 @@ public class ComponentBuilder {
         return this;
     }
 
-    public ComponentBuilder addResolver(@NotNull final ResolverType type, @NotNull final TagResolver... tags) {
+    /**
+     * Adds a resolver type to the list.
+     *
+     * @param type the {@link ResolverType}
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder addResolver(@NotNull final ResolverType type, @NotNull final TagResolver... tags) {
         final List<TagResolver> resolvers = this.resolvers.getOrDefault(type, new ArrayList<>());
 
         resolvers.addAll(Arrays.asList(tags));
@@ -132,30 +226,67 @@ public class ComponentBuilder {
         return this;
     }
 
-    public ComponentBuilder removeResolver(@NotNull final ResolverType type) {
+    /**
+     * Removes a resolver type from the list.
+     *
+     * @param type the {@link ResolverType}
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder removeResolver(@NotNull final ResolverType type) {
         this.resolvers.remove(type);
 
         return this;
     }
 
-    public ComponentBuilder addLine(@NotNull final String line) {
+    /**
+     * Adds a line to the list.
+     *
+     * @param line the line to add
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder addLine(@NotNull final String line) {
         this.lines.add(line);
 
         return this;
     }
 
-    public ComponentBuilder removeLine(@NotNull final String line) {
+    /**
+     * Removes a line from the list.
+     *
+     * @param line the line to remove
+     * @return {@link ComponentBuilder}
+     */
+    public @NotNull final ComponentBuilder removeLine(@NotNull final String line) {
         this.lines.remove(line);
 
         return this;
     }
 
+    /**
+     * A collection of resolver types
+     *
+     * @author ryderbelserion
+     */
     public enum ResolverType {
+        /**
+         * Placeholder resolver type
+         */
         PLACEHOLDER(),
+        /**
+         * Click event resolver type
+         */
         CLICK_EVENT(),
+        /**
+         * Hover event resolver type
+         */
         HOVER_EVENT(),
+        /**
+         * Text color resolver type
+         */
         TEXT_COLOR(),
-
+        /**
+         * Generic resolver type
+         */
         GENERIC_TYPE()
     }
 }
