8675ffdb12f84807cf0979e328e2c1e50b8f4396
Ryder Belserion <contact@ryderbelserion.com>
2025-07-19 00:30:53 -0400

    Add required methods for parsing/coloring or checking if things are enabled.

--- core/src/main/java/com/ryderbelserion/fusion/core/FusionCore.java
+++ core/src/main/java/com/ryderbelserion/fusion/core/FusionCore.java
@@ -2,6 +2,12 @@ package com.ryderbelserion.fusion.core;
 
 import com.ryderbelserion.fusion.core.api.exceptions.FusionException;
 import com.ryderbelserion.fusion.core.api.interfaces.IFusionCore;
+import com.ryderbelserion.fusion.core.api.support.ModManager;
+import com.ryderbelserion.fusion.core.utils.StringUtils;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
@@ -11,20 +17,43 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public abstract class FusionCore implements IFusionCore {
 
+    private final StringUtils stringUtils;
+    private final ModManager modManager;
+
     protected FusionConfig config;
+
     private Path dataPath;
 
     public FusionCore(@NotNull final Consumer<FusionCore> consumer) {
         consumer.accept(this);
+
+        this.stringUtils = new StringUtils();
+        this.modManager = new ModManager(this);
     }
 
+    public abstract Component parse(@NotNull final Audience audience, @NotNull final String message, @NotNull final Map<String, String> placeholders, @NotNull final List<TagResolver> tags);
+
+    public Component parse(@NotNull final Audience audience, @NotNull final String message, @NotNull final Map<String, String> placeholders) {
+        return parse(audience, message, placeholders, List.of());
+    }
+
+    public Component parse(@NotNull final Audience audience, @NotNull final String message) {
+        return parse(audience, message, new HashMap<>());
+    }
+
+    public abstract String papi(@NotNull final Audience audience, @NotNull final String message);
+
     public abstract FusionCore init(@NotNull final Consumer<FusionCore> consumer);
+
+    public abstract boolean isModReady(@NotNull final Key key);
 
     public abstract FusionCore reload();
 
@@ -100,7 +129,19 @@ public abstract class FusionCore implements IFusionCore {
     }
 
     @Override
+    public ModManager getModManager() {
+        return this.modManager;
+    }
+
+    @Override
+    public StringUtils getStringUtils() {
+        return this.stringUtils;
+    }
+
+    @Override
     public void setDataPath(@NotNull final Path dataPath) {
+        if (this.dataPath != null) return;
+
         this.dataPath = dataPath;
     }
 
--- paper/src/main/java/com/ryderbelserion/fusion/paper/FusionPaper.java
+++ paper/src/main/java/com/ryderbelserion/fusion/paper/FusionPaper.java
@@ -2,21 +2,36 @@ package com.ryderbelserion.fusion.paper;
 
 import com.ryderbelserion.fusion.core.FusionConfig;
 import com.ryderbelserion.fusion.core.FusionCore;
+import com.ryderbelserion.fusion.core.api.support.ModSupport;
 import com.ryderbelserion.fusion.core.files.enums.FileAction;
 import com.ryderbelserion.fusion.core.files.enums.FileType;
 import com.ryderbelserion.fusion.core.files.types.YamlCustomFile;
 import com.ryderbelserion.fusion.paper.files.PaperFileManager;
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class FusionPaper extends FusionCore {
 
     private final PaperFileManager fileManager;
+    private final PluginManager pluginManager;
 
     public FusionPaper(@NotNull final JavaPlugin plugin) {
         super(consumer -> {
@@ -28,6 +43,24 @@ public class FusionPaper extends FusionCore {
         init(consumer -> {
             this.config = new FusionConfig(this.fileManager.getYamlFile(Key.key("fusion")));
         });
+
+        final Server server = plugin.getServer();
+
+        this.pluginManager = server.getPluginManager();
+    }
+
+    @Override
+    public Component parse(@NotNull final Audience audience, @NotNull final String message, @NotNull final Map<String, String> placeholders, @NotNull final List<TagResolver> tags) {
+        final List<TagResolver> resolvers = new ArrayList<>(tags);
+
+        placeholders.forEach((key, value) -> resolvers.add(Placeholder.parsed(getStringUtils().replaceAllBrackets(key).toLowerCase(), value)));
+
+        return MiniMessage.miniMessage().deserialize(papi(audience, message), !resolvers.isEmpty() ? TagResolver.resolver(resolvers) : TagResolver.empty()).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE);
+    }
+
+    @Override
+    public String papi(@NotNull final Audience audience, @NotNull final String message) {
+        return audience instanceof Player player && getModManager().getMod(ModSupport.placeholder_api).isEnabled() ? PlaceholderAPI.setPlaceholders(player, message) : message;
     }
 
     @Override
@@ -56,6 +89,16 @@ public class FusionPaper extends FusionCore {
     }
 
     @Override
+    public boolean isModReady(@NotNull final Key key) {
+        return this.pluginManager.isPluginEnabled(key.value());
+    }
+
+    @Override
+    public PaperFileManager getFileManager() {
+        return this.fileManager;
+    }
+
+    @Override
     public FusionConfig getConfig() {
         return this.config;
     }
@@ -65,10 +108,5 @@ public class FusionPaper extends FusionCore {
         this.config.reload();
 
         return this;
-    }
-
-    @Override
-    public PaperFileManager getFileManager() {
-        return this.fileManager;
     }
 }
