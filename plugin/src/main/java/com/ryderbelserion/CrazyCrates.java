f70d41061492418731a8b8eb04718050e3428b0b
Ryder Belserion <contact@ryderbelserion.com>
2025-08-08 13:17:26 -0400

    Test paper file manager

--- plugin/src/main/java/com/ryderbelserion/CrazyCrates.java
+++ plugin/src/main/java/com/ryderbelserion/CrazyCrates.java
@@ -2,11 +2,15 @@ package com.ryderbelserion;
 
 import ch.jalu.configme.SettingsManager;
 import com.ryderbelserion.fusion.core.files.FileManager;
+import com.ryderbelserion.fusion.core.files.enums.FileAction;
 import com.ryderbelserion.fusion.core.files.types.JaluCustomFile;
 import com.ryderbelserion.fusion.paper.FusionPaper;
+import com.ryderbelserion.fusion.paper.files.PaperFileManager;
+import com.ryderbelserion.fusion.paper.files.types.PaperCustomFile;
 import com.ryderbelserion.keys.ConfigKeys;
 import com.ryderbelserion.listeners.ItemListener;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import java.nio.charset.Charset;
@@ -20,9 +24,10 @@ public class CrazyCrates extends JavaPlugin {
     public void onEnable() {
         this.fusion = new FusionPaper(this);
 
-        final FileManager fileManager = this.fusion.getFileManager();
+        final PaperFileManager fileManager = this.fusion.getFileManager();
 
-        fileManager.addFile(getDataPath().resolve("config.yml"),
+        fileManager.addPaperFile(getDataPath().resolve("test.yml"), consumer -> consumer.addAction(FileAction.EXTRACT_FILE))
+                .addFile(getDataPath().resolve("config.yml"),
                 options -> options.indentationSize(2).charset(Charset.defaultCharset()),
                 builder -> builder.configurationData(ConfigKeys.class));
 
@@ -34,6 +39,16 @@ public class CrazyCrates extends JavaPlugin {
             final SettingsManager config = customFile.get().getConfiguration();
 
             logger.warn("Test: {}", config.getProperty(ConfigKeys.test));
+        }
+
+        final Optional<PaperCustomFile> paperCustomFile = fileManager.getPaperFile(getDataPath().resolve("test.yml"));
+
+        if (paperCustomFile.isPresent()) {
+            final PaperCustomFile file = paperCustomFile.get();
+
+            final YamlConfiguration configuration = file.getConfiguration();
+
+            logger.warn("Configuration: {}", configuration.getString("test-value", "N/A"));
         }
 
         getServer().getPluginManager().registerEvents(new ItemListener(), this);
