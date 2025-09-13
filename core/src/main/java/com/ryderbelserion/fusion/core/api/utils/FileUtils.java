61c640da236f44543bd28b454393e5ea08f6a6d8
Ryder Belserion <contact@ryderbelserion.com>
2025-09-13 10:46:39 -0400

    Add extra logging to the exists check

--- core/src/main/java/com/ryderbelserion/fusion/core/api/utils/FileUtils.java
+++ core/src/main/java/com/ryderbelserion/fusion/core/api/utils/FileUtils.java
@@ -54,6 +54,8 @@ public class FileUtils {
         }
 
         if (Files.exists(content)) {
+            fusion.log("info", "Cannot extract folder {} to {}, because it already exists @ {}.", input, output, content);
+
             return;
         }
 
