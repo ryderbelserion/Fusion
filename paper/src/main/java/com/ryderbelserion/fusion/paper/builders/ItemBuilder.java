001fa4682f2615452e400a4964c5e234e3d1b829
Ryder Belserion <contact@ryderbelserion.com>
2025-09-15 10:34:24 -0400

    Add methods without the consumer for ItemBuilder

--- paper/src/main/java/com/ryderbelserion/fusion/paper/builders/ItemBuilder.java
+++ paper/src/main/java/com/ryderbelserion/fusion/paper/builders/ItemBuilder.java
@@ -15,6 +15,14 @@ public class ItemBuilder extends BaseItemBuilder<ItemBuilder> {
         super(itemType, consumer);
     }
 
+    public ItemBuilder(@NotNull final ItemType itemType, final int amount) {
+        super(itemType, amount, consumer -> {});
+    }
+
+    public ItemBuilder(@NotNull final ItemType itemType) {
+        this(itemType, 1);
+    }
+
     public ItemBuilder(@NotNull final ItemStack itemStack) {
         super(itemStack);
     }
