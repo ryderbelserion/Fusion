cc5a0c47eb9270bc89949b0b2eaf118c280c0b73
Ryder Belserion <contact@ryderbelserion.com>
2026-05-01 20:11:59 -0400

    Fix command chaining

--- minecraft/commands/src/main/java/com/ryderbelserion/fusion/commands/CommandManager.java
+++ minecraft/commands/src/main/java/com/ryderbelserion/fusion/commands/CommandManager.java
@@ -1,5 +1,7 @@
 package com.ryderbelserion.fusion.commands;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.ryderbelserion.fusion.commands.annotations.Leaf;
 import com.ryderbelserion.fusion.commands.api.TreeCommand;
 import com.ryderbelserion.fusion.commands.processor.TreeProcessor;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +15,11 @@ public abstract class CommandManager<S> {
     public void parse(@NotNull final TreeCommand tree) {
         final TreeProcessor<S> root = tree.getProcessor();
 
-        root.process(tree).processTree(root.getClass().getDeclaredMethods());
+        final LiteralArgumentBuilder<S> builder = root.process(tree).getBuilder();
+
+        for (final Leaf leaf : root.processTree(tree.getClass().getDeclaredMethods())) {
+            builder.then(LiteralArgumentBuilder.literal(leaf.value()));
+        }
 
         for (final Object index : tree.getCommands()) {
             root.process(index);
