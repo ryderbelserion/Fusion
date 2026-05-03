ab39b60db9ea34ddbc1c22326160e3cf5010c83d
Ryder Belserion <contact@ryderbelserion.com>
2026-05-02 22:39:43 -0400

    Remove unused method

--- minecraft/commands/src/main/java/com/ryderbelserion/fusion/commands/api/TreeProcessor.java
+++ minecraft/commands/src/main/java/com/ryderbelserion/fusion/commands/api/TreeProcessor.java
@@ -2,28 +2,14 @@ package com.ryderbelserion.fusion.commands.api;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.ryderbelserion.fusion.commands.CommandManager;
-import com.ryderbelserion.fusion.commands.api.annotations.subs.Leaf;
 import com.ryderbelserion.fusion.commands.api.objects.types.BranchCommand;
-import com.ryderbelserion.fusion.commands.api.objects.types.LeafCommand;
 import com.ryderbelserion.fusion.commands.api.objects.types.TreeCommand;
 import org.jetbrains.annotations.NotNull;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 
 public class TreeProcessor<S> {
 
     private LiteralArgumentBuilder<S> builder;
     private String description;
-
-    public @NotNull List<LeafCommand<S>> processTree(@NotNull final CommandManager<S> commandManager, @NotNull final Method[] methods) {
-        return Arrays.stream(methods)
-                .filter(insect -> insect.isAnnotationPresent(Leaf.class))
-                .sorted(Comparator.comparingInt(insect -> insect.getAnnotation(Leaf.class).weight()))
-                .map(map -> new LeafCommand<>(commandManager, map, map.getAnnotation(Leaf.class)))
-                .toList();
-    }
 
     public @NotNull TreeProcessor processBranch(@NotNull final CommandManager<S> manager, @NotNull final Object object) {
         final BranchCommand<S> command = new BranchCommand<>(object);
