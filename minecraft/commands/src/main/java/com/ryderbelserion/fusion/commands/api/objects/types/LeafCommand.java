95f1db768bfc322de47a1fd0ddb119781bc1460d
Ryder Belserion <contact@ryderbelserion.com>
2026-05-03 01:35:15 -0400

    Add sender type from parameters

--- minecraft/commands/src/main/java/com/ryderbelserion/fusion/commands/api/objects/types/LeafCommand.java
+++ minecraft/commands/src/main/java/com/ryderbelserion/fusion/commands/api/objects/types/LeafCommand.java
@@ -8,34 +8,47 @@ import com.ryderbelserion.fusion.commands.api.annotations.subs.Leaf;
 import org.jetbrains.annotations.NotNull;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class LeafCommand<S> {
 
-    private final CommandManager<S> commandManager;
+    private final CommandManager<S> manager;
+    private final Parameter[] parameters;
     private final String permission;
     private final Method method;
     private final String leaf;
 
-    public LeafCommand(@NotNull final CommandManager<S> commandManager, @NotNull final Method method, @NotNull final Leaf leaf) {
-        this.commandManager = commandManager;
-
+    public LeafCommand(@NotNull final CommandManager<S> manager, @NotNull final Method method, @NotNull final Leaf leaf) {
+        this.permission = method.isAnnotationPresent(Permission.class) ? method.getAnnotation(Permission.class).permission() : "";
+        this.leaf = leaf.value();
+        this.manager = manager;
         this.method = method;
 
-        this.permission = this.method.isAnnotationPresent(Permission.class) ? this.method.getAnnotation(Permission.class).permission() : "";
-
-        this.leaf = leaf.value();
+        this.parameters = method.getParameters();
     }
 
     public @NotNull final LiteralArgumentBuilder<S> execute(@NotNull final Object object) {
         final LiteralArgumentBuilder<S> builder = LiteralArgumentBuilder.literal(this.leaf);
 
         if (!this.permission.isBlank()) {
-            builder.requires(context -> this.commandManager.hasPermission(context, this.permission));
+            builder.requires(context -> this.manager.hasPermission(context, this.permission));
         }
 
         builder.executes(_ -> invoke(this.method, object));
 
         return builder;
+    }
+
+    public @NotNull Class<? extends S> getSender() {
+        final Parameter[] parameters = this.method.getParameters();
+
+        if (parameters.length == 0) {
+            throw new IllegalStateException("No sender parameter found.");
+        }
+
+        final Class<?> type = parameters[0].getType();
+
+        return (Class<? extends S>) type;
     }
 
     public int invoke(@NotNull final Method method, @NotNull final Object object) {
@@ -52,10 +65,6 @@ public class LeafCommand<S> {
 
     public @NotNull final String getPermission() {
         return this.permission;
-    }
-
-    public @NotNull final Method getMethod() {
-        return this.method;
     }
 
     public @NotNull final String getLeaf() {
