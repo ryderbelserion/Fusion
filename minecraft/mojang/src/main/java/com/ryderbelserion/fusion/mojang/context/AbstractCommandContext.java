a1e73b619cc0c416384980b7e7cff8d59748bbaa
Ryder Belserion <contact@ryderbelserion.com>
2026-05-22 19:26:06 -0400

    Make argument fetchers optionals

--- minecraft/mojang/src/main/java/com/ryderbelserion/fusion/mojang/context/AbstractCommandContext.java
+++ minecraft/mojang/src/main/java/com/ryderbelserion/fusion/mojang/context/AbstractCommandContext.java
@@ -2,6 +2,7 @@ package com.ryderbelserion.fusion.mojang.context;
 
 import com.mojang.brigadier.context.CommandContext;
 import org.jspecify.annotations.NonNull;
+import java.util.Optional;
 
 public abstract class AbstractCommandContext<S> {
 
@@ -11,20 +12,20 @@ public abstract class AbstractCommandContext<S> {
         this.context = context;
     }
 
-    public @NonNull final String getStringArgument(@NonNull final String key) {
-        return this.context.getArgument(key, String.class);
+    public @NonNull final Optional<String> getStringArgument(@NonNull final String key) {
+        return Optional.ofNullable(this.context.getArgument(key, String.class));
     }
 
-    public final double getDoubleArgument(@NonNull final String key) {
-        return this.context.getArgument(key, Double.class);
+    public final Optional<Double> getDoubleArgument(@NonNull final String key) {
+        return Optional.ofNullable(this.context.getArgument(key, Double.class));
     }
 
-    public final int getIntegerArgument(@NonNull final String key) {
-        return this.context.getArgument(key, Integer.class);
+    public final Optional<Integer> getIntegerArgument(@NonNull final String key) {
+        return Optional.ofNullable(this.context.getArgument(key, Integer.class));
     }
 
-    public final float getFloatArgument(@NonNull final String key) {
-        return this.context.getArgument(key, Float.class);
+    public final Optional<Float> getFloatArgument(@NonNull final String key) {
+        return Optional.ofNullable(this.context.getArgument(key, Float.class));
     }
 
     public @NonNull final CommandContext<S> getContext() {
